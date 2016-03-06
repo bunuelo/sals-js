64a273604e3f94f0d5709cae986a12cfb58a8d99
Bo Morgan <bo.morgan@bomorgan.com>
2016-03-06 13:30:15 -0800

    rewriting scripts

---
+++ js/NipsSalsIndex.js
@@ -0,0 +1,20 @@
+
+console.log("Loading NipsSalsIndex.js");
+
+window.onload = function() {
+    console.log("Executing document.onload");
+    document.body.innerHTML = "Hi Bob.";
+    var script_file_names = ["scripts/NipsSalsCore.js",
+			     "scripts/NipsSalsGraph.js"];
+    for (var i in script_file_names) {
+	(function() {
+	    var script_file_name = script_file_names[i];
+	    var script           = document.createElement("script");
+	    script.src           = script_file_name;
+	    script.onload        = function() {console.log("Loaded " + script_file_name);};
+	    script.onerror       = function() {console.log("Error loading " + script_file_name);};
+	    document.body.appendChild(script);
+	})();
+    }
+};
+
--- makefile
+++ makefile
@@ -1,8 +1,25 @@
-default: compile-webpage
+default: print-usage
+
+EDITOR = emacs
+
+print-usage:
+	@echo "make <command>"
+	@echo ""
+	@echo "  print-usage: Prints this usage message."
+	@echo "  start-editor: Opens source files in text editor."
+	@echo "  compile-webpage: Compiles the required source files into the ./www directory."
+	@echo "  upload-webpage: Compiles and uploads the ./www director to bomorgan.com"
+	@echo ""
+
+start-editor:
+	$(EDITOR) makefile html/*.html js/*.js
 
 compile-webpage:
 	mkdir -p ./www/
 	mkdir -p ./www/scripts/
 	cp ./html/*.html ./www/
 	cp ./js/*.js ./www/scripts/
+	chmod -R a+rx ./www/
 
+upload-webpage: compile-webpage
+	rsync -avz ./ bomorgan@bomorgan.com:bomorgan/nips-sals/
