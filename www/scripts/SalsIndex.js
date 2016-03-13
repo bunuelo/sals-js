be32e0496d43056fb9f1021210425ef8f52665d2
Bo Morgan <bo.morgan@bomorgan.com>
2016-03-13 16:39:53 -0700

    editing...

--- www/scripts/SalsIndex.js
+++ www/scripts/SalsIndex.js
@@ -5,11 +5,15 @@ window.onload = function() {
     console.log("Executing window.onload");
     var success_callback = function() {
 	try {
-	    document.body.innerHTML = "!! Successfully loaded all files." + "<br>" + sals.go.test();
+	    document.body.innerHTML = "Successfully loaded all files." + "<br>" + sals.go.test();
 	} catch (error) {
-	    console.log("SalsCore Error: " + error.message + "\n" + error.stack);
+	    console.log("Sals Error: " + error.message + "\n" + error.stack);
 	}
     };
-    sals.core.initialize(success_callback);
+    try {
+	sals.core.initialize(success_callback);
+    } catch (error) {
+	console.log("Sals Error: " + error.message + "\n" + error.stack);
+    }
 };
 
