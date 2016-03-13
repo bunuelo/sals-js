e972713c16f19894c1d2548b6d66c1b0572acd0b
Bo Morgan <bo.morgan@bomorgan.com>
2016-03-13 16:38:53 -0700

    editing...

--- js/SalsIndex.js
+++ js/SalsIndex.js
@@ -5,7 +5,7 @@ window.onload = function() {
     console.log("Executing window.onload");
     var success_callback = function() {
 	try {
-	    document.body.innerHTML = "Successfully loaded all files." + "<br>" + sals.go.test();
+	    document.body.innerHTML = "!! Successfully loaded all files." + "<br>" + sals.go.test();
 	} catch (error) {
 	    console.log("SalsCore Error: " + error.message + "\n" + error.stack);
 	}
