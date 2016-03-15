a6054d8882f0fd48a5f7e668c37176d75593f9d6
Bo Morgan <bo.morgan@bomorgan.com>
2016-03-14 21:05:06 -0700

    editing...

--- www/scripts/SalsVis.js
+++ www/scripts/SalsVis.js
@@ -48,7 +48,7 @@ sals.vis.vis_graph__new = function(width, height, graph) {
     };
     var options = {
 	physics : {
-	    stabilizations : false
+	    stabilization : false
 	},
 	layout : {
 	    improvedLayout : false
