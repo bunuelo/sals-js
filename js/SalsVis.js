9a796f9116a8b66d9e4f342bf44c091566c4eb93
Bo Morgan <bo.morgan@bomorgan.com>
2016-03-14 21:03:48 -0700

    editing...

--- js/SalsVis.js
+++ js/SalsVis.js
@@ -5,20 +5,6 @@ sals.vis.vis_graph__new = function(width, height, graph) {
     var graph__edges    = sals.graph.graph__edges(graph);
     var vis_nodes_array = [];
     var vis_edges_array = [];
-    //var vis_nodes_array = [
-    //	{id: 1, label: 'Node 1'},
-    //	{id: 2, label: 'Node 2'},
-    //	{id: 3, label: 'Node 3'},
-    //	{id: 4, label: 'Node 4'},
-    //	{id: 5, label: 'Node 5'}
-    //];
-    //var vis_edges_array = [
-    //	{from: 1, to: 3},
-    //	{from: 1, to: 2},
-    //	{from: 2, to: 4},
-    //	{from: 2, to: 5}
-    //];
-    
     sals.frame.frame__foreach_key(graph__nodes, function(graph__node__uid) {
 	var graph__node        = sals.frame.frame__get_element(graph__nodes, graph__node__uid);
 	var graph__node__label = sals.graph.graph_node__label(graph__node);
@@ -61,6 +47,9 @@ sals.vis.vis_graph__new = function(width, height, graph) {
 	edges: edges
     };
     var options = {
+	physics : {
+	    stabilizations : false
+	},
 	layout : {
 	    improvedLayout : false
 	}
