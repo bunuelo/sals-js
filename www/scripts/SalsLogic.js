231bbf2903ab419f88100a2ee5cc2d2c5fee1b87
Bo Morgan <bo.morgan@bomorgan.com>
2016-04-24 20:05:30 -0700

    editing...

--- www/scripts/SalsLogic.js
+++ www/scripts/SalsLogic.js
@@ -1,20 +1,38 @@
 
 sals.logic = {};
 
-(function() { // preedicate BEGIN
-    var object_type = sals.object.object_type__new("predicate");
+(function() { // predicate_type BEGIN
+    var object_type = sals.object.object_type__new("predicate_type");
     sals.object_registry.add_type(object_type);
     
-    sals.logic.predicate__new = function() {
-	self = sals.object.object__new("predicate");
+    sals.logic.predicate_type__new = function(verb_transitive, parameter_frame) {
+	self = sals.object.object__new("predicate_type");
+	sals.frame__add_element(self, "verb_transitive", verb_transitive);
+	sals.frame__add_element(self, "parameter_frame", parameter_frame);
 	return self;
     };
     
-    sals.logic.predicate__is_type = function(exp) {
-	return sals.object.object__is_type(exp, "predicate");
+    sals.logic.predicate_type__is_type = function(exp) {
+	return sals.object.object__is_type(exp, "predicate_type");
     };
     
-})(); // predicate END
+    sals.logic.predicate_type__verb_transitive = function(self) {
+	return sals.frame__get_element(self, "verb_transitive");
+    };
+    
+    sals.logic.predicate_type__set_verb_transitive = function(self, value) {
+	return sals.frame__set_element(self, "verb_transitive", value);
+    };
+    
+    sals.logic.predicate_type__parameter_frame = function(self) {
+	return sals.frame__get_element(self, "parameter_frame");
+    };
+    
+    sals.logic.predicate_type__set_parameter_frame = function(self, value) {
+	return sals.frame__set_element(self, "parameter_frame", value);
+    };
+    
+})(); // predicate_type END
 
 sals.logic.test_logic = function() {
     console.log("test_logic here.");
