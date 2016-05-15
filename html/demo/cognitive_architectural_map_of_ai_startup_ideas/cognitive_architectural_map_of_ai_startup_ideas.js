5e61fc208a9d42322c87c0fc42d8576b6aa66f90
Bo Morgan <bo.morgan@bomorgan.com>
2016-05-15 13:45:39 -0700

    editing...

--- html/demo/cognitive_architectural_map_of_ai_startup_ideas/cognitive_architectural_map_of_ai_startup_ideas.js
+++ html/demo/cognitive_architectural_map_of_ai_startup_ideas/cognitive_architectural_map_of_ai_startup_ideas.js
@@ -59,8 +59,8 @@ window.onload = function() {
     console.log("Executing window.onload");
     var success_callback = function() {
 	try {
-	    var width  = 450;
-	    var height = 400;
+	    var width   = window.innerWidth;
+	    var height  = window.innerHeight;
 	    var element = sals.demo.ai_startup_idea.new_ai_startup_idea_dom_element(width, height);
 	    document.body.appendChild(element);
 	} catch (error) {
