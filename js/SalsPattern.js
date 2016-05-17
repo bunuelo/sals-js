1e991f6ecd2f5747f447406e012cff2b9a7957bc
Bo Morgan <bo.morgan@bomorgan.com>
2016-05-16 20:24:39 -0700

    editing...

--- js/SalsPattern.js
+++ js/SalsPattern.js
@@ -155,31 +155,31 @@ sals.pattern = {};
     
 })(); // pattern_implies END
 
-(function() { // knowledge_base
-    var object_type = sals.object.object_type__new("knowledge_base");
+(function() { // pattern_set
+    var object_type = sals.object.object_type__new("pattern_set");
     sals.object_registry.add_type(object_type);
     
-    sals.pattern.knowledge_base__new = function(expressions) {
-	var self = sals.object.object__new("knowledge_base");
+    sals.pattern.pattern_set__new = function(expressions) {
+	var self = sals.object.object__new("pattern_set");
 	sals.frame.frame__add_element(self, "expressions", expressions);
 	return self;
     };
     
-    sals.pattern.knowledge_base__is_type = function(exp) {
-	return sals.object.object__is_type(exp, "knowledge_base");
+    sals.pattern.pattern_set__is_type = function(exp) {
+	return sals.object.object__is_type(exp, "pattern_set");
     };
     
-    sals.pattern.knowledge_base__expressions = function(self) {
+    sals.pattern.pattern_set__expressions = function(self) {
 	return sals.frame.frame__get_element(self, "expressions");
     };
     
-    sals.pattern.knowledge_base__set_expressions = function(self, value) {
+    sals.pattern.pattern_set__set_expressions = function(self, value) {
 	sals.frame.frame__set_element(self, "expressions", value);
     };
     
-    sals.pattern.knowledge_base__to_string = function(self) {
-	var expressions         = sals.pattern.knowledge_base__expressions(self);
-	var to_string           = "(knowledge_base ";
+    sals.pattern.pattern_set__to_string = function(self) {
+	var expressions         = sals.pattern.pattern_set__expressions(self);
+	var to_string           = "(pattern_set ";
 	var expressions__length = expressions.length;
 	var expressions__index  = 0;
 	while (expressions__index < expressions__length) {
@@ -194,29 +194,39 @@ sals.pattern = {};
 	return to_string;
     };
     
-})(); // knowledge_base END
+})(); // pattern_set END
 
 sals.pattern.expression__to_string = function(expression) {
     if      (sals.pattern.pattern_variable__is_type(expression)) {return sals.pattern.pattern_variable__to_string(expression);}
     else if (sals.pattern.pattern__is_type(expression))          {return sals.pattern.pattern__to_string(expression);}
     else if (sals.pattern.pattern_and__is_type(expression))      {return sals.pattern.pattern_and__to_string(expression);}
     else if (sals.pattern.pattern_implies__is_type(expression))  {return sals.pattern.pattern_implies__to_string(expression);}
+    else if (sals.pattern.pattern_set__is_type(expression))      {return sals.pattern.pattern_set__to_string(expression);}
     return "\"" + expression + "\"";
 };
 
 sals.pattern.test_pattern = function() {
     sals.core.log("test_pattern HERE.");
-    var x       = sals.pattern.pattern__new_from_string("aristotle is a man");
-    var y       = sals.pattern.pattern__new_from_string("all mans are mortal");
-    var z       = sals.pattern.pattern__new_from_string("aristotle is mortal");
-    var and     = sals.pattern.pattern_and__new([x, y]);
-    var implies = sals.pattern.pattern_implies__new(and, z);
-    var gx       = sals.pattern.pattern__new([sals.pattern.pattern_variable__new("X"), "a", sals.pattern.pattern_variable__new("Y")]);
-    var gy       = sals.pattern.pattern__new(["all", sals.pattern.pattern_variable__new("Y"), "s are ", sals.pattern.pattern_variable__new("Z")]);
-    var gz       = sals.pattern.pattern__new([sals.pattern.pattern_variable__new("X"), sals.pattern.pattern_variable__new("Z")]);
-    var gand     = sals.pattern.pattern_and__new([gx, gy]);
-    var gimplies = sals.pattern.pattern_implies__new(gand, gz);
-    sals.core.log("implies = " + sals.pattern.expression__to_string(implies));
-    sals.core.log("gimplies = " + sals.pattern.expression__to_string(gimplies));
+    (function() {
+	var x       = sals.pattern.pattern__new_from_string("aristotle is a man");
+	var y       = sals.pattern.pattern__new_from_string("all mans are mortal");
+	var z       = sals.pattern.pattern__new_from_string("aristotle is mortal");
+	var and     = sals.pattern.pattern_and__new([x, y]);
+	var implies = sals.pattern.pattern_implies__new(and, z);
+	sals.core.log("implies = " + sals.pattern.expression__to_string(implies));
+    })();
+    (function() {
+	var gx       = sals.pattern.pattern__new([sals.pattern.pattern_variable__new("X"), "a", sals.pattern.pattern_variable__new("Y")]);
+	var gy       = sals.pattern.pattern__new(["all", sals.pattern.pattern_variable__new("Y"), "s are ", sals.pattern.pattern_variable__new("Z")]);
+	var gz       = sals.pattern.pattern__new([sals.pattern.pattern_variable__new("X"), sals.pattern.pattern_variable__new("Z")]);
+	var gand     = sals.pattern.pattern_and__new([gx, gy]);
+	var gimplies = sals.pattern.pattern_implies__new(gand, gz);
+	sals.core.log("gimplies = " + sals.pattern.expression__to_string(gimplies));
+    })();
+    (function() {
+	var pattern_set = sals.pattern.pattern_set__new([sals.pattern.pattern__new(["aristotle is a man"]),
+							 sals.pattern.pattern__new(["all mans are mortal"])]);
+	sals.core.log("pattern_set = " + sals.pattern.expression__to_string(pattern_set));
+    })();
     sals.core.log("test_pattern DONE.");
 };
