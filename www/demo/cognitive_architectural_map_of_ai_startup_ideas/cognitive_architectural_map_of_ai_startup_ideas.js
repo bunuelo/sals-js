dc62c08c004cb264bfc56056fd4b502ba90173bf
Bo Morgan <bo.morgan@bomorgan.com>
2016-05-22 15:28:25 -0700

    editing...

--- www/demo/cognitive_architectural_map_of_ai_startup_ideas/cognitive_architectural_map_of_ai_startup_ideas.js
+++ www/demo/cognitive_architectural_map_of_ai_startup_ideas/cognitive_architectural_map_of_ai_startup_ideas.js
@@ -6,6 +6,34 @@ if (typeof(sals["demo"]) == "undefined") {
 
 sals.demo.ai_startup_idea = {};
 
+{ // propogate_node_data BEGIN
+    
+    sals.propogate_node_data__new = function() {
+	var self = sals.frame.frame__new();
+	sals.frame.frame__add_element(self, "source_frame", sals.frame.frame__new());
+	return self;
+    };
+    
+    sals.propogate_node_data__source_frame = function(self) {
+	return sals.frame.frame__get_element(self, "source_frame");
+    };
+    
+    sals.propogate_node_data__set_source_frame = function(self, value) {
+	return sals.frame.frame__set_element(self, "source_frame", value);
+    };
+    
+    sals.propogate_node_data__set_source_value = function(self, source_key, value) {
+	var source_frame = sals.propogate_node_data__source_frame(self);
+	sals.frame.frame__set_element(source_frame, source_key, value);
+    };
+    
+    sals.propogate_node_data__get_source_value = function(self, source_key) {
+	var source_frame = sals.propogate_node_data__source_frame(self);
+	return sals.frame.frame__get_element(source_frame, source_key);
+    };
+    
+} // propogate_node_data END
+
 sals.demo.ai_startup_idea.graph__add_concept_line = function(graph,
 							     node_concept_map,
 							     relationship,
@@ -97,34 +125,6 @@ sals.demo.ai_startup_idea.graph__add_concept_line = function(graph,
 	}
     })();
 };
-
-{ // propogate_node_data BEGIN
-    
-    sals.propogate_node_data__new = function() {
-	var self = sals.frame.frame__new();
-	sals.frame.frame__add_element(self, "source_frame", sals.frame.frame__new());
-	return self;
-    };
-    
-    sals.propogate_node_data__source_frame = function(self) {
-	return sals.frame.frame__get_element(self, "source_frame");
-    };
-    
-    sals.propogate_node_data__set_source_frame = function(self, value) {
-	return sals.frame.frame__set_element(self, "source_frame", value);
-    };
-    
-    sals.propogate_node_data__set_source_value = function(self, source_key, value) {
-	var source_frame = sals.propogate_node_data__source_frame(self);
-	sals.frame.frame__set_element(source_frame, source_key, value);
-    };
-    
-    sals.propogate_node_data__get_source_value = function(self, source_key) {
-	var source_frame = sals.propogate_node_data__source_frame(self);
-	return sals.frame.frame__get_element(source_frame, source_key);
-    };
-    
-} // propogate_node_data END
 
 sals.demo.ai_startup_idea.propogation__iterate = function(graph) {
     sals.core.log("iterate HERE.");
