6ab83cf9709e4b9a7cb3032a2d5b8633b72aa398
Bo Morgan <bo.morgan@bomorgan.com>
2016-05-24 20:19:10 -0700

    editing...

--- www/demo/cognitive_architectural_map_of_ai_startup_ideas/cognitive_architectural_map_of_ai_startup_ideas.js
+++ www/demo/cognitive_architectural_map_of_ai_startup_ideas/cognitive_architectural_map_of_ai_startup_ideas.js
@@ -430,9 +430,9 @@ sals.demo.ai_startup_idea.new_ai_startup_idea_dom_element = function(width, heig
 	    edges.push([home_entertainment_market, parent_market_relationship, consumer_product_market]);
 	}
 	{
-	    var wearable_market = "Wearable\nMarket";
-	    minor_markets.push(wearable_market);
-	    edges.push([wearable_market, parent_market_relationship, consumer_product_market]);
+	    var personal_health_monitor_market = "Personal\nHealth Monitor\nMarket";
+	    minor_markets.push(personal_health_monitor_market);
+	    edges.push([personal_health_monitor_market, parent_market_relationship, consumer_product_market]);
 	}
     }
     // market END
@@ -767,7 +767,7 @@ sals.demo.ai_startup_idea.new_ai_startup_idea_dom_element = function(width, heig
     {
 	var wearable_technology_product = "Wearable Technology\nProduct";
 	products.push(wearable_technology_product);
-	edges.push([wearable_technology_product, sold_in_market_relationship, wearable_market]);
+	edges.push([wearable_technology_product, sold_in_market_relationship, personal_health_monitor_market]);
     }
     {
 	var health_monitor_wearable_technology_product = "Health Monitor\nWearable Technology\nProduct";
