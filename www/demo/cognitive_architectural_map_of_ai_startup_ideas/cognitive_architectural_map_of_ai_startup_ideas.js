b7ffb2105c952e6c64029712ef1958eeae48e5ee
Bo Morgan <bo.morgan@bomorgan.com>
2016-05-24 19:09:19 -0700

    editing...

--- www/demo/cognitive_architectural_map_of_ai_startup_ideas/cognitive_architectural_map_of_ai_startup_ideas.js
+++ www/demo/cognitive_architectural_map_of_ai_startup_ideas/cognitive_architectural_map_of_ai_startup_ideas.js
@@ -58,16 +58,19 @@ sals.demo.ai_startup_idea = {};
 	var g                         = sals.frame.frame__get_element(propogate_node_data_frame, "g");
 	var b                         = sals.frame.frame__get_element(propogate_node_data_frame, "b");
 	(function() { // red stripe
-	    var red_stripe_r      = 0.75;
-	    var red_stripe_g      = 1.0;
-	    var red_stripe_b      = 0.75;
-	    var red_stripe_radius = 0.1;
+	    var red_stripe_enabled = false;
+	    var red_stripe_r       = 0.75;
+	    var red_stripe_g       = 1.0;
+	    var red_stripe_b       = 0.75;
+	    var red_stripe_radius  = 0.1;
 	    if ((r >= (red_stripe_r - red_stripe_radius)) && (r <= (red_stripe_r + red_stripe_radius)) &&
 		(g >= (red_stripe_g - red_stripe_radius)) && (g <= (red_stripe_g + red_stripe_radius)) &&
 		(b >= (red_stripe_b - red_stripe_radius)) && (b <= (red_stripe_b + red_stripe_radius))) {
-		r = 1.0;
-		g = 0.0;
-		b = 0.0;
+		if (red_stripe_enabled) {
+		    r = 1.0;
+		    g = 0.0;
+		    b = 0.0;
+		}
 	    }
 	})();
 	sals.vis.object__soft_merge_recursive(vis_node, {
