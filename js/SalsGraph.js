1b56a4b5ca67f1a6c4c99bf99b205064b4a57002
Bo Morgan <bo.morgan@bomorgan.com>
2016-07-11 20:17:22 -0700

    editing...

--- js/SalsGraph.js
+++ js/SalsGraph.js
@@ -166,7 +166,7 @@ sals.graph.graph__to_predicate_set = function(self) {
 		edge__to_node__label__str = "" + edge__to_node__label;
 	    }
 	    (function() { // the green block to be on the table
-		var predicate_type = sals.logic.predicate_type__new(edge__label, sals.frame.frame({"subject" : "an object", "direct object" : sals.logic.parameter_type__new("an object")}));
+		var predicate_type = sals.logic.predicate_type__new(edge__label, sals.frame.frame({"subject" : sals.logic.parameter_type__new("an object"), "direct object" : sals.logic.parameter_type__new("an object")}));
 		var predicate      = sals.logic.predicate__new(predicate_type, sals.frame.frame({"subject" : edge__from_node__label, "direct object" : edge__to_node__label__str}));
 		sals.logic.predicate_set__add_predicate(predicate_set, predicate)
 	    })();
