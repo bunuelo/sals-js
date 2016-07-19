a58ab3c8c31d4ea6441af0f04174908e13ed39f3
Bo Morgan <bo.morgan@bomorgan.com>
2016-07-18 19:26:12 -0700

    editing...

--- www/scripts/SalsMath.js
+++ www/scripts/SalsMath.js
@@ -7,3 +7,16 @@ sals.math.floor = function(x) {
 sals.math.abs = function(x) {
     return Math.abs(x);
 };
+
+// returns floating point number randomly sampled from a uniform distribution over the range from 0 to 1
+sals.math.random = function() {
+    return Math.random();
+};
+
+sals.math.random_int = function(superior_index) {
+    var value = sals.math.floor(Math.random() * (superior_index));
+    if (value == superior_index) {
+	value = superior_index - 1;
+    }
+    return value;
+};
